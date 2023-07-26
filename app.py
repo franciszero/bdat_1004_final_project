757d473aef0093330386be05a680e89fd0a93f32
Francis Chan <franciszero@gmail.com>
2023-07-26 12:56:36 -0400

    .

--- app.py
+++ app.py
@@ -1,44 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# 设置连接数据库的URL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@127.0.0.1:3306/test'
 
-@app.route("/")
-def hello():
-    return "<p>Hello 111</p>"
-
-
-@app.route("/json")
-def foo():
-    return "xxx"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# 查询时会显示原始SQL语句
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
 
-@app.route("/webpage")
-def webpage():
-    return render_template("1.html")
+class Role(db.Model):
+    # 定义表名
+    __tablename__ = 'roles'
+    # 定义列对象
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    user = db.relationship('User', backref='role')
+
+    # repr()方法显示一个可读字符串
+    def __repr__(self):
+        return 'Role:%s' % self.name
 
 
-#
-# @app.getdocpic("/getdog")
-# def getdog():
-#     return render_template("1.html")
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True)
+    password = db.Column(db.String(64))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return 'User:%s' % self.name
 
 
-# see: https://github.com/rbigelow/flask-intro/blob/main/app.py
-@app.route("/ross")
-def ross():
-    return "<p> Hi I'm Ross. </p><img src='https://bigelow.cc/wp-content/uploads/sites/3/cropped-ross-bigelow.png'> "
+class Role(db.Model):
+    ...
+    #关键代码
+    user = db.relationship('User', backref='role', lazy='dynamic')
+    ...
+
+class User(db.Model):
+    ...
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 
-myData = {
-    "name": 'Ross Bigelow',
-    "dream": "To inspire others to fulfil thier dreams.",
-    'age': 1,
-    'birthdate': "2020-01-01"
-}
-
-
-@app.route("/json")
-def myJson():
-    return myData
-
+if __name__ == '__main__':
+    app.run(debug=True)
