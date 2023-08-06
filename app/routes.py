2dc6466c879c8d645c3fc452bc5663299fbc0c60
Francis Chan <franciszero@gmail.com>
2023-08-06 02:36:30 -0400

    .

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, jsonify
 from app.models import WeatherData
 from flask import request
-from sqlalchemy import desc
+from sqlalchemy import desc, asc
 from datetime import datetime, timedelta
 
 main = Blueprint('main', __name__)
@@ -20,7 +20,8 @@ def index():
     # read data from database
     # weather_data = WeatherData.query.filter_by(city=city).order_by(desc(WeatherData.date)).all()
     days_ago_14 = datetime.now() - timedelta(days=14)
-    weather_data = WeatherData.query.filter_by(city=city).filter(WeatherData.date >= days_ago_14).order_by(desc(WeatherData.date)).all()
+    weather_data = WeatherData.query.filter_by(city=city).filter(WeatherData.date >= days_ago_14).order_by(
+        asc(WeatherData.date)).all()
 
     # return an HTML page to web browser
     html = render_template('index.html', weather_data=weather_data, cities=cities, city=city)
