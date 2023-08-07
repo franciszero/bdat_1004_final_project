c39b93856d8b349a2854417a75a676c6345290f1
Francis Chan <franciszero@gmail.com>
2023-08-06 21:06:50 -0400

    .

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import time
 from threading import Thread
 
@@ -39,4 +40,4 @@ if __name__ == '__main__':
     scheduler_thread = Thread(target=run_scheduler, args=(app,))
     scheduler_thread.start()
 
-    app.run(debug=False, host='0.0.0.0', port=5002)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
